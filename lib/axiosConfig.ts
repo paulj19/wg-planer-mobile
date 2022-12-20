98f8669c29050325dfec6939c7a3d7e25e6f2b53
paul <diljosepaul@gmail.com>
2022-12-21 00:24:06 +0530

    axiosRetry initial version

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,20 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 import axiosRetry from 'axios-retry';
 
-axiosRetry(axios, {retries: 3});
+//axiosRetry(axios, {retries: 3});
 
 // const MockAdapter = require("axios-mock-adapter");
 
 // const mock = new MockAdapter(axios);
 
-axiosRetry(axios, {
-    retryDelay: (retryCount) => {
-        return retryCount * 1000;
-    }
-});
 
 const client = axios.create({baseURL: 'http://localhost:8080'});
-axiosRetry(client, {retries: 3});
+axiosRetry(client, {
+    retryDelay: (retryCount) => {
+        console.log(`retry attempt: ${retryCount}`);
+        return retryCount * 1000;
+    },
+    retryCondition: (error) => {
+    // if retry condition is not specified, by default idempotent requests are retried
+    return 500 <= error.response.status < 600;
+    },
+    retries: 3
+});
 
 // mock.onPost("/login").reply(200, {
 //     id: '1',
