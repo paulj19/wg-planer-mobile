36323b70225802f844d52e26836aa815a795a19d
paul <diljosepaul@gmail.com>
2023-03-04 20:13:36 +0100

    refacs, add AuthToken

--- lib/UrlPaths.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL_DEV = "http://localhost:8080";
-export const PATH_VALIDATE_ACCESS_TOKEN = "/validate-access-token";
-export const PATH_LOGIN = "/login";
-export const URL_AUTH_SERVER = "https://wg-planer-auth-server.onrender.com";
--- util/storage/AsyncStorage.ts
+++
@@ -1,6 +0,0 @@
-import Storage from "react-native-storage";
-
-export const aStorage = new Storage({
-    storageBackend: window.localStorage,
-    defaultExpires: null,
-})
