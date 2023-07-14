55398d55e557c00b8471bd39707b4deed306a3a9
paul <diljosepaul@gmail.com>
2023-07-14 19:31:02 +0200

    update HomeScreen

--- src/Home/HomeScreen.tsx
+++ src/Home/HomeScreen.tsx
@@ -1,18 +1,17 @@
 import Logout from "../login/Logout";
 import { Text, View } from "react-native";
-import { useGetUserProfileQuery} from "../features/user/UserSlice";
-// import {useGetXXXQuery } from "../features/api/apiSlice";
+import { useGetUserProfileQuery } from "../features/user/UserSlice";
 
 export default function HomeScreen() {
-  const { data: userprofile, isLoading, isSuccess } = useGetUserProfileQuery();
-  // const { data: xxx} = useGetXXXQuery();
-  console.log("USER", userprofile);
-  // console.log("xxx", xxx);
-  return (
+  const { data: userprofile } = useGetUserProfileQuery();
+  if (userprofile) {
+    return (
     <View>
-      {}
-      <Text>{"home screen"}</Text>
-      <Logout />
+        <Text>{"hello " + userprofile.username + "!"}</Text>
+        <Logout />
     </View>
-  );
+    );
+  } else {
+    throw Error("userprofile must not be null");
+  }
 }
