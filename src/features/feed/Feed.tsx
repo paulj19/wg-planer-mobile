967d3f281ae925209e73557ab61f50136161373b
paul <diljosepaul@gmail.com>
2024-06-27 15:05:36 +0530

    feed basic structure done

--- src/features/feed/Feed.tsx
+++ src/features/feed/Feed.tsx
@@ -5,34 +5,53 @@ import NewTaskCard from "features/feed/NewTaskCard";
 import { Text, ScrollView } from "react-native";
 import { useGetPostLoginInfoQuery } from "features/user/UserSlice";
 import { useSelector } from "react-redux";
-import { createSelector } from '@reduxjs/toolkit'
-import {FloorItem, Task} from "types/types";
-
+import { createSelector } from "@reduxjs/toolkit";
+import { FloorItem, Task } from "types/types";
 
 export function Feed(): ReactElement {
-  // console.log("MYTAKS", myTasks);
-  // const user = useSelector(state => selectUserById(state, userId))
+  const sortCriteria = (a, b) =>
+    a.Reminders === b.Reminders
+      ? a.AssignmentDate - b.AssignmentDate
+      : b.Reminders - a.Reminders;
   const selectUserTasksById = useMemo(() => {
-    const emptyArray = []
-    //todo remove refetchOnFocus
+    const emptyArray = [];
     return createSelector(
-      result => result?.data?.floor,
+      (result) => result?.data?.floor,
       (state, userId) => userId,
-      (floor: FloorItem, userId) => ({...floor, Tasks: floor?.Tasks?.filter((task: Task) => task.AssignedTo === userId).sort((a, b) => (a.Reminders === b.Reminders ? (a.AssignmentDate - b.AssignmentDate): (b.Reminders - a.Reminders))) || emptyArray}))
-  }, [])
+      (floor: FloorItem, userId) => ({
+        ...floor,
+        Tasks:
+          floor?.Tasks?.filter(
+            (task: Task) => task.AssignedTo === userId
+          ).sort(sortCriteria) || emptyArray,
+      })
+    );
+  }, []);
 
-  const { floorInfo} = useGetPostLoginInfoQuery(undefined, {refetchOnFocus: true, selectFromResult: result => ({ ...result, floorInfo: selectUserTasksById(result, result.data?.userprofile?.id) }) })
+  const { floorInfo } = useGetPostLoginInfoQuery(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      floorInfo: selectUserTasksById(result, result.data?.userprofile?.id),
+    }),
+  });
 
   return (
     <ScrollView>
       {floorInfo?.Tasks?.map((task) => (
         <TaskCardFeed taskName={task.Name} reminders={task.Reminders} />
       ))}
-      { floorInfo?.Feed?.map((feedItem) => {
+      {floorInfo?.Feed?.map((feedItem) => {
         if (feedItem.Type === "NEW_RESIDENT") {
-          return <NewResidentCard name={feedItem.NewResident} room={feedItem.Room} />;
+          return (
+            <NewResidentCard name={feedItem.NewResident} room={feedItem.Room} />
+          );
         } else if (feedItem.Type === "NEW_TASK") {
-          return <NewTaskCard creator={feedItem.Creator} taskName={feedItem.TaskName} />;
+          return (
+            <NewTaskCard
+              creator={feedItem.Creator}
+              taskName={feedItem.TaskName}
+            />
+          );
         }
       })}
     </ScrollView>
