9693e7aabdf4845e58b89a357eda699d7e54408e
paul <diljosepaul@gmail.com>
2024-07-17 14:51:47 +0530

    msw working, web navigation not working, staring with cypress

--- src/mocks/AuthHandlers.js
+++ src/mocks/AuthHandlers.js
@@ -1,12 +1,12 @@
 import { MOCK_BASE_URL, URL_INTROSPECT_TOKEN } from "util/UrlPaths";
-import { rest } from "msw";
+import { http, HttpResponse } from 'msw'
 
 const withMockBaseUrl = (path) => {
   return new URL(path, MOCK_BASE_URL).toString();
 }
 
 export const authHandlers = [
-  rest.post(URL_INTROSPECT_TOKEN, (req, res, ctx) => {
-    return res(ctx.json({active: true}), ctx.status(200));
+  http.get(URL_INTROSPECT_TOKEN, () => {
+    return HttpResponse.json({active: true})
   }),
 ];
--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,6 +1,14 @@
 import { authHandlers } from "./AuthHandlers";
 import { userHandlers } from "./UserHandlers";
 import { setupServer } from "msw/native";
+import { setupWorker } from "msw/browser";
+import { isDevicePhoneOrTablet } from "util/Device";
 
+let mswHost
+if (isDevicePhoneOrTablet()) {
+  mswHost = setupServer(...authHandlers, ...userHandlers);
+} else {
+  mswHost = setupWorker(...authHandlers, ...userHandlers);
+}
 
-export const server = setupServer(...authHandlers, ...userHandlers);
+export { mswHost };
