72fb58cd2f30950b164caf43d75f4cc5d861f7b9
paul <diljosepaul@gmail.com>
2024-09-05 13:31:51 +0530

    befor paris

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ export default function App() {
             }}
           >
             <>
-              {!authState?.signedIn ? (
+              {authState?.signedIn ? (
                 <>
                   <Stack.Screen
                     name="Home"
